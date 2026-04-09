1241dbce941fd943b68cf80b9677d737fd9bfd24
Павел Труфанов <ittp.pro@gmail.com>
2026-04-09 05:23:38 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/dashboard/navigation-sidebar.tsx
+++ src/components/dashboard/navigation-sidebar.tsx
@@ -34,35 +34,31 @@ export function NavigationSidebar() {
         <SidebarMenu>
           {menuItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref legacyBehavior>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                >
-                  <a>
-                    <item.icon />
-                    <span>{item.label}</span>
-                  </a>
-                </SidebarMenuButton>
-              </Link>
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                tooltip={item.label}
+              >
+                <Link href={item.href}>
+                  <item.icon />
+                  <span>{item.label}</span>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
           <SidebarSeparator />
           {fullscreenMenuItems.map((item) => (
              <SidebarMenuItem key={item.href}>
-               <Link href={item.href} passHref legacyBehavior>
-                 <SidebarMenuButton
-                   asChild
-                   isActive={pathname === item.href}
-                   tooltip={item.label}
-                 >
-                   <a>
-                     <item.icon />
-                     <span>{item.label}</span>
-                   </a>
-                 </SidebarMenuButton>
-               </Link>
+               <SidebarMenuButton
+                 asChild
+                 isActive={pathname === item.href}
+                 tooltip={item.label}
+               >
+                 <Link href={item.href}>
+                   <item.icon />
+                   <span>{item.label}</span>
+                 </Link>
+               </SidebarMenuButton>
              </SidebarMenuItem>
           ))}
         </SidebarMenu>
